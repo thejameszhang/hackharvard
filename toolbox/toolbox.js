50ec8bec1d8a5d8f822096ef2c6093f8a550f93e
Sng Hong <sng_hong@outlook.com>
2022-10-15 14:56:28 -0400

    toolbar is done

--- toolbox/toolbox.js
+++ toolbox/toolbox.js
@@ -1,4 +1,20 @@
-dragElement(document.getElementsByClassName("draggable-toolbox"));
+console.log("Toolbox.js loaded");
+// For sticky navbar
+window.onscroll = function() {adjustY()};
+
+// Get the navbar
+var sb = document.getElementById("sidebar");
+
+// Get the offset position of the navbar
+var sticky = sb.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function adjustY() {
+  document.getElementById("sidebar").style.top = window.pageYOffset;
+  console.log(window.pageYOffset);
+  console.log(sticky);
+}
+dragElement(sidebar);
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
