b982bbd4dbdd800411745a06d6053f70b88238a6
Sng Hong <sng_hong@outlook.com>
2022-10-15 15:03:19 -0400

    fixed merge conflicts

--- popup.js
+++ popup.js
@@ -1,12 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var rejectElements = document.getElementsByClassName('.reject');
-    for (i = 0; i < rejectElements.length; i++) {
-        rejectElements[i].addEventListener("click", closeMain);
-    }
-    var acceptElements = document.getElementsByClassName(".accept");
-    for (i = 0; i < rejectElements.length; i++) {
-        rejectElements[i].addEventListener("click", openSidebar);
-    }
+    document.getElementById("reject").addEventListener("click", closeMain);
+    document.getElementById("accept").addEventListener("click", openSidebar);
   });
 function closeMain(element) {
     window.close();
