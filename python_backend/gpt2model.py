26c36ba5a8316fde0c547fb6a6859bee66be6039
James Zhang <james.yi.zhang@gmail.com>
2022-10-15 12:25:44 -0400

    gpt-2 abstractive model

--- python_backend/gpt2model.py
+++ python_backend/gpt2model.py
@@ -1,16 +1,18 @@
 # Importing model and tokenizer
-from transformers import XLMWithLMHeadModel, XLMTokenizer
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
+#this needs to be data driven - whatever the user highlights/wants to zoom in or out of
 original_text = ""
 
-# Instantiating the model and tokenizer 
-tokenizer = XLMTokenizer.from_pretrained('xlm-mlm-en-2048')
-model = XLMWithLMHeadModel.from_pretrained('xlm-mlm-en-2048')
+# Instantiating the model and tokenizer with gpt-2
+tokenizer = GPT2Tokenizer.from_prerained('gpt2')
+model=GPT2LMHeadModel.from_pretrained('gpt2')
 
-# Encoding text to get input ids & pass them to model.generate()
+# Encoding text to get input ids & pass them to model.generate() - this max length param should
+# also be customized
 inputs = tokenizer.batch_encode_plus([original_text],return_tensors='pt',max_length=512)
-summary_ids = model.generate(inputs['input_ids'],early_stopping=True)
+summary_ids=model.generate(inputs['input_ids'],early_stopping=True)
 
-# Decode and print the summary
-XLM_summary = tokenizer.decode(summary_ids[0],skip_special_tokens=True)
-print(XLM_summary)
+# Decoding and printing summary
+GPT_summary = tokenizer.decode(summary_ids[0],skip_special_tokens=True)
+print(GPT_summary)
