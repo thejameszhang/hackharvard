12fb71d781aefe6518e1d47cef5c7e691a4a8016
James Zhang <james.yi.zhang@gmail.com>
2022-10-15 12:32:57 -0400

    fixed typo in model

--- python_backend/gpt2model.py
+++ python_backend/gpt2model.py
@@ -6,12 +6,12 @@ original_text = ""
 
 # Instantiating the model and tokenizer with gpt-2
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model=GPT2LMHeadModel.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 # Encoding text to get input ids & pass them to model.generate() - this max length param should
 # also be customized
 inputs = tokenizer.batch_encode_plus([original_text],return_tensors='pt',max_length=512)
-summary_ids=model.generate(inputs['input_ids'],early_stopping=True)
+summary_ids = model.generate(inputs['input_ids'],early_stopping=True)
 
 # Decoding and printing summary
 GPT_summary = tokenizer.decode(summary_ids[0],skip_special_tokens=True)
