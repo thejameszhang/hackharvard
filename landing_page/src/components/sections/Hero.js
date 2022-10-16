8303ef74d8a2fcab13d4be2bf8af0a200490ae72
Sng Hong <sng_hong@outlook.com>
2022-10-15 22:28:43 -0400

    updated landing page

--- landing_page/src/components/sections/Hero.js
+++ landing_page/src/components/sections/Hero.js
@@ -61,18 +61,18 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Never miss another <span className="text-color-primary">detail</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+              We summarize terms and conditions for you, so you only have read the important bits.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
+                    Install Chrome Extension
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                  <Button tag="a" color="dark" wideMobile href="https://github.com/snghong/hackharvard">
                     View on Github
                     </Button>
                 </ButtonGroup>
