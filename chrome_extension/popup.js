e699b5bb2a33bb826896fb0edfdf74e449e52ca8
James Zhang <james.yi.zhang@gmail.com>
2023-06-21 15:44:32 -0600

    clean up popup js

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,24 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Call the appropriate function when a button is clicked.
     document.getElementById("reject").addEventListener("click", closeMain);
     document.getElementById("accept").addEventListener("click", openSidebar);
   });
 
-  function closeMain(element) {
+function closeMain(element) {
     window.close();
-};
+};    
+
 async function getCurrentTab() {
+    /*
+    This function gets the current tab and only the current tab
+    because of the below query options.
+    Args:
+    Returns:
+        A promise, `tabs.Tab` instance or `undefined`  
+    */
     let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
-  }
+}
+
 async function openSidebar(element) {
-    // send message to content script
+    /*
+    This functions opens the toolbox sidebar and closes the default popup.
+    */
+    // Send a message to content script.
     console.log("open sidebar");
+    // Wait for getCurrentTab() to return the current tab.
     const tab = await getCurrentTab();
+    // 
     chrome.scripting.executeScript({
-        target: { tabId: tab.id },
+        target: {tabId: tab.id},
         files: ['content_script.js','toolbox/toolbox.js', 'text.js']
-      });
+      }).then(() => comsole.log("javascript injected"));
+    // Once the sidebar is open, we can close default popup.
     window.close();
 }
